14ab31463074db92c3c45b57bcc4774fa864f00b
Nele Albers <nele.96@web.de>
2024-05-23 17:33:06 +0200

    changed password

--- actions/definitions.py
+++ actions/definitions.py
@@ -3,6 +3,6 @@ Store definitions used in rasa actions (e.g., related to database).
 """
 
 DATABASE_HOST = "mysql"
-DATABASE_PASSWORD = "password"
+DATABASE_PASSWORD = "mysql_delibot_rot_green"
 DATABASE_PORT = 3306
 DATABASE_USER = "root"
